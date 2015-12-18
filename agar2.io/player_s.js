31f1bac915f55fe27b250295cc58f5d5565cde4f
minecrafter200 <rowankodsihall@me.com>
2015-12-18 15:51:47 -0500

    Update player_s.js

--- agar2.io/player_s.js
+++ agar2.io/player_s.js
@@ -2,7 +2,7 @@ var player_s = {}
 player_s.init = function(scene) {
 
       var geometry = new THREE.CircleGeometry( 8, 32);
-			var material = new THREE.MeshPhongMaterial( { map : texture } );
+			var material = new THREE.MeshPhongMaterial( { map } );
       var circle = new THREE.Mesh( geometry, material );
       circle.position.z = 0.5
       scene.add(circle)
