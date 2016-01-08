b5a6097710b5d44eb0845c65f029279f86d78ebf
minecrafter200 <rowankodsihall@me.com>
2016-01-08 16:26:16 -0500

    Update player_s.js

--- agar2.io/player_s.js
+++ agar2.io/player_s.js
@@ -2,7 +2,7 @@ var player_s = {}
 player_s.init = function(scene, mass) {
       var color = new THREE.Color(Math.random(),Math.random(),Math.random());
       
-      var geometry = new THREE.CircleGeometry( mass, 64);
+      var geometry = new THREE.CircleGeometry( ((mass)*10), 64);
 			var material = new THREE.MeshPhongMaterial()
 			material.color = color
       var circle = new THREE.Mesh( geometry, material );
