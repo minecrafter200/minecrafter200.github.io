56d95994eaf85f22fe54bbd5d992cdfe6187a663
minecrafter200 <rowankodsihall@me.com>
2016-01-22 16:33:24 -0500

    Update player_s.js

--- agar2.io/player_s.js
+++ agar2.io/player_s.js
@@ -25,7 +25,7 @@ function onMouseMove( event ) {
 	mh = mouseh
 	mx = mousex
 theta = Math.asin(mx/mh)
-console.log("t:" + theta + "     x:" + mx + "     y:" + my)
+console.log("t:" + theta + "     x:" + mx + "     h:" + mh)
 	
 
 }
