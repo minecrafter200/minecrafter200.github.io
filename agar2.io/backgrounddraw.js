2a4afc67516b415d5a50bc50aa4827766a45b9e7
minecrafter200 <rowankodsihall@me.com>
2016-01-29 15:51:00 -0500

    Created

---
+++ agar2.io/backgrounddraw.js
@@ -0,0 +1,13 @@
+var material = new THREE.LineBasicMaterial({
+	color: 0x0000ff
+});
+
+var geometry = new THREE.Geometry();
+geometry.vertices.push(
+	new THREE.Vector3( -10, 0, 0 ),
+	new THREE.Vector3( 0, 10, 0 ),
+	new THREE.Vector3( 10, 0, 0 )
+);
+
+var line = new THREE.Line( geometry, material );
+scene.add( line );
