dd74576b62c7d73cbc3b7455e9c2ca56e6a4110f
goodwinj14 <goodwinj14@yahoo.com>
2016-01-29 10:24:33 -0500

    Update player_s.js

--- agar2.io/player_s.js
+++ agar2.io/player_s.js
@@ -36,9 +36,9 @@ console.log("Theta " + theta);
 player_s.visual = function() {
 	
 	//circle.rotation.z = theta
-	if(theta>null){
+	//if(theta>null){
 	circle.rotation.set(new THREE.Vector3( 0, 0, Math.PI/2));
-	}
+	//}
 	circle.translateY(1);
 	
 	// need to set z to mass. (+mass)
