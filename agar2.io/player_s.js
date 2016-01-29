1cd6d7dc5400db2643d67da1673bbf57cca35bc2
goodwinj14 <goodwinj14@yahoo.com>
2016-01-29 10:52:01 -0500

    Update player_s.js

--- agar2.io/player_s.js
+++ agar2.io/player_s.js
@@ -21,10 +21,10 @@ var priorY = 0;
 var mouseh;
 function onMouseMove( event ) {
 
-	delta_x = (( event.clientX / (window.innerWidth * 0.8) ) ) - circle.position.x;
+	delta_x = (( event.clientX / (window.innerWidth * 0.8) ) );
 	priorX = (( event.clientX / (window.innerWidth * 0.8) ) );
 	
-	delta_y = (( event.clientY / (window.innerWidth * 0.8) ) ) - circle.position.y;
+	delta_y = (( event.clientY / (window.innerWidth * 0.8) ) );
 	var priorY = - (( event.clientY / (window.innerHeight * 0.8) ) );	
 	mouseh = Math.pow(delta_x,2)+Math.pow(delta_y,2);
 	
