e79aaafa4922211a89f2672e6985a3f5136ce928
goodwinj14 <goodwinj14@yahoo.com>
2016-02-12 15:24:03 -0500

    Update player_s.js

--- agar2.io/player_s.js
+++ agar2.io/player_s.js
@@ -26,6 +26,7 @@ function onMouseMove( event ) {
 	if(delta_y<0&&delta_x>0){
 		theta = Math.PI + ((Math.PI/2)-theta);
 	}if(delta_y<0&&delta_x<0){
+		console.log("called");
 		theta = Math.PI + theta;
 	}
 	
