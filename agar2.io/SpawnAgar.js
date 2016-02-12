bc8e76682a2bd277cb0e3f81667fa6ad25c9ae15
goodwinj14 <goodwinj14@yahoo.com>
2016-02-12 16:55:47 -0500

    Update SpawnAgar.js

--- agar2.io/SpawnAgar.js
+++ agar2.io/SpawnAgar.js
@@ -2,12 +2,13 @@ var Agar = {}
 var circle = null;
 var camera2
 Agar.initSpawn = function(){
-var color = new THREE.Color(Math.random(),Math.random(),Math.random());
+
 	
      for(i=0;i<3000;i++) {
-     	x=(Math.random()-Math.random())*((1/2)*11180.3)
-     		y=(Math.random()-Math.random())*((1/2)*11180.3)
-      var geometry = new THREE.CircleGeometry( ((mass)*10), 64);
+     	x=(Math.random()-Math.random())*((1/2)*11180.3);
+     	y=(Math.random()-Math.random())*((1/2)*11180.3);
+     	var color = new THREE.Color(Math.random(),Math.random(),Math.random());
+      var geometry = new THREE.CircleGeometry( (1), 64);
 			var material = new THREE.MeshPhongMaterial()
 			material.color = color
       circle = new THREE.Mesh( geometry, material );
