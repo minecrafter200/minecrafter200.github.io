d5a99c15a93d5ba4350d346d42ec8346b3dbf1ba
minecrafter200 <rowankodsihall@me.com>
2016-02-12 16:49:47 -0500

    Update SpawnAgar.js

--- agar2.io/SpawnAgar.js
+++ agar2.io/SpawnAgar.js
@@ -3,11 +3,19 @@ var circle = null;
 var camera2
 Agar.initSpawn = function{
 var color = new THREE.Color(Math.random(),Math.random(),Math.random());
-      while(i<3000, i)
+	
+     for(i=0;i<3000;i++) {
+     	x=(Math.random()-Math.random())*((1/2)*11180.3)
+     		y=(Math.random()-Math.random())*((1/2)*11180.3)
       var geometry = new THREE.CircleGeometry( ((mass)*10), 64);
 			var material = new THREE.MeshPhongMaterial()
 			material.color = color
       circle = new THREE.Mesh( geometry, material );
-      circle.position.z = 0.5
+      circle.position.z = 0.4
+      circle.position.x = x
+      circle.position.y = y
+      
       scene.add(circle)
+     
+     }
 }
