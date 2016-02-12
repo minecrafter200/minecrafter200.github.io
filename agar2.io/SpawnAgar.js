6be8397e17e4db3bdca9ce411f73c8c90cc35a5a
minecrafter200 <rowankodsihall@me.com>
2016-02-12 16:29:47 -0500

    Update SpawnAgar.js

--- agar2.io/SpawnAgar.js
+++ agar2.io/SpawnAgar.js
@@ -1,1 +1,8 @@
-
+var color = new THREE.Color(Math.random(),Math.random(),Math.random());
+      
+      var geometry = new THREE.CircleGeometry( ((mass)*10), 64);
+			var material = new THREE.MeshPhongMaterial()
+			material.color = color
+      circle = new THREE.Mesh( geometry, material );
+      circle.position.z = 0.5
+      scene.add(circle)
