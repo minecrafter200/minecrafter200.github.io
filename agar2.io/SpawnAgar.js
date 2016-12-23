41333471217b4edeb73ee173573a9d1862d91ea8
minecrafter200 <rowankodsihall@me.com>
2016-12-23 16:11:01 -0500

    Update SpawnAgar.js

--- agar2.io/SpawnAgar.js
+++ agar2.io/SpawnAgar.js
@@ -5,13 +5,13 @@ Agar.initSpawn = function(){
 
 	
      for(i=0;i<749;i++) {
-     	x=(Math.random()*100);
-     	y=(Math.random()*100);
-	     z=(Math.random()*100);
+     	x=(Math.random()*90);
+     	y=(Math.random()*90);
+	     z=(Math.random()*90);
      	var color = new THREE.Color(Math.random(),Math.random(),Math.random());
      	
       var geometry = new THREE.SphereGeometry( 0.25, 32, 32 );
-			var material = new THREE.MeshPhongMaterial()
+			var material = new THREE.MeshNormalMaterial()
 			material.color = color
       Sphere = new THREE.Mesh( geometry, material );
       Sphere.position.z = z
