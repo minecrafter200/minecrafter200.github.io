a171b140a6a71e6dd77673c5ee28d0756aeb6a34
minecrafter200 <rowankodsihall@me.com>
2017-01-06 16:26:46 -0500

    Update map.js

--- agar2.io/map.js
+++ agar2.io/map.js
@@ -3,5 +3,5 @@ map.grid = {};
 map.grid.points = new Array();
 map.grid.init = new function(){
 for(i=0;i<1000000;i++)
-  map.grid.points[i] = new THREE.Vector3;
+  map.grid.points[i] = new THREE.Vector3(0,0,0);
 }
