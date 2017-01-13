dc5942cb5f5089701e49d39c2735e35e1b9131c6
minecrafter200 <rowankodsihall@me.com>
2017-01-13 15:42:17 -0500

    Update map.js

--- agar2.io/map.js
+++ agar2.io/map.js
@@ -1,12 +1,13 @@
 var map = {}
 map.grid = {};
-map.grid.points:int[,,,]; //new Array(new Array(new Array()));
+map.grid.points = new Array();
 map.grid.init = function(){
+  int i = 0;
 for(z=0;z<100;z++){
   for(y=0;y<100;y++){
     for(x=0;x<100;x++){
-       //map.grid.points[i] = new THREE.Vector3((x*10)-500,(y*10)-500,(z*10)-500);
-      map.grid.points[z][y][x] = 2; //new THREE.Vector3((x*10)-500,(y*10)-500,(z*10)-500);
+      map.grid.points[i] = new THREE.Vector3((x*10)-500,(y*10)-500,(z*10)-500);
+    i++
     }
   }
   
