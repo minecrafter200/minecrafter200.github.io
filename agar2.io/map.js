ce87e1547b48d6dad4ddaa96aaa508a7aff62565
goodwinj14 <goodwinj14@yahoo.com>
2017-01-13 15:18:00 -0500

    Update map.js

--- agar2.io/map.js
+++ agar2.io/map.js
@@ -1,6 +1,6 @@
 var map = {}
 map.grid = {};
-map.grid.points = new Array();
+map.grid.points = new Array(new Array(new Array()));
 map.grid.init = function(){
 for(z=0;z<100;z++){
   for(y=0;y<100;y++){
