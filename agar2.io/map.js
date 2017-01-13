0bda8f378052e9c47ba007526091b552c17e7610
minecrafter200 <rowankodsihall@me.com>
2017-01-13 15:58:05 -0500

    Update map.js

--- agar2.io/map.js
+++ agar2.io/map.js
@@ -5,6 +5,7 @@ map.grid.init = function(){
 var curve = new THREE.CatmullRomCurve3([
   new THREE.Vector3( -10, 0, -10 )
 ]);
+console.log(curve)
 var i = 0;
 for(z=0;z<100;z++){
   for(y=0;y<100;y++){
