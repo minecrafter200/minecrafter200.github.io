4557f239e5c428b2344739af8f42b0cc6b1c2d18
minecrafter200 <rowankodsihall@me.com>
2017-01-20 15:27:38 -0500

    Update map.js

--- agar2.io/map.js
+++ agar2.io/map.js
@@ -21,10 +21,12 @@ console.log(curve)
 var i = 0;
 for(z=0;z<100;z++){
   for(y=0;y<100;y++){
+    
     for(x=0;x<100;x++){
       map.grid.points[i] = new THREE.Vector3((x*10)-500,(y*10)-500,(z*10)-500);
     i++
     }
+    map.lines.add(new THREE.CatmullRomCurve3( map.grid.points.slice((y*100),((y*100)+99) ))));
   }
   
   }
