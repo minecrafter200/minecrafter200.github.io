1d9d482972a12d7b37a026eddb8dae3ddb8ead59
minecrafter200 <rowankodsihall@me.com>
2017-02-10 16:01:54 -0500

    Update map.js

--- agar2.io/map.js
+++ agar2.io/map.js
@@ -33,7 +33,7 @@ var slice1 = new THREE.Object3D();
       
 
 
-      temp2.position.z = temp3.position.z+1000
+      temp2.position.z = temp3.position.z+100
       var temp4 = temp2.clone();
       slice1.add(temp4)
       temp3=temp2
