0ee1f6a1182cbba5b719d4d0f31809c498ff93ed
minecrafter200 <rowankodsihall@me.com>
2017-03-03 16:24:09 -0500

    Update

--- agar2.io/map.js
+++ agar2.io/map.js
@@ -1,5 +1,6 @@
 var map = {}
 map.grid = {};
+map.grid.points = new Array();
 map.grid.init = function(){
   var xfragment = "uniform vec3 color;"+
       "uniform float opacity;"+
@@ -27,34 +28,10 @@ map.grid.init = function(){
     vertexShader: xvertext,
     fragmentShader: xfragment,
     blending: THREE.AdditiveBlending,
-    depthTest: false
-    transparent: true;
+    depthTest: false,
+    transparent: true
   });
   var geometry = new THREE.SphereGeometry(2,40,40);
  var vertices = geometry.vertices
  
- 
- var buffergeometry = new THREE.BufferGeometry();
-
-			var position = new THREE.Float32BufferAttribute( vertices.length * 3, 3 ).copyVector3sArray( vertices );
-			buffergeometry.addAttribute( 'position', position );
-
-			var displacement = new THREE.Float32BufferAttribute( vertices.length * 3, 3 );
-			buffergeometry.addAttribute( 'displacement', displacement );
-
-			var customColor = new THREE.Float32BufferAttribute( vertices.length * 3, 3 );
-			buffergeometry.addAttribute( 'customColor', customColor );
-
-			var color = new THREE.Color( 0xffffff );
-
-			for( var i = 0, l = customColor.count; i < l; i ++ ) {
-
-				color.setHSL( i / l, 0.5, 0.5 );
-				color.toArray( customColor.array, i * customColor.itemSize );
-
-			}
-
-			object = new THREE.Line( buffergeometry, shaderMaterial );
-			object.rotation.x = 0.2;
-			map.lines = object;
 }
